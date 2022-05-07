fc3fb7ca29205182264c8be01222fcc693c26c08
DPolikarpov <polikarpov.denv.2@gmail.com>
2022-05-07 20:51:00 +0300

    STACKS-9: Add ListView with three Containers.

--- lib/main.dart
+++ lib/main.dart
@@ -32,16 +32,40 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        backgroundColor: const Color(0xFF121212),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        appBar: AppBar(
+          title: Text(widget.title),
+          backgroundColor: const Color(0xFF121212),
         ),
-      ),
-    );
+        body: ListView(
+          children: [
+            Container(
+              height: 50,
+              color: Colors.red,
+            ),
+            Container(
+              height: 30,
+              child: const Text("Some text"),
+            ),
+            Container(
+              height: 200,
+              decoration: BoxDecoration(
+                  shape: BoxShape.rectangle,
+                  color: const Color(0x4FFD0000),
+                  borderRadius: BorderRadiusDirectional.circular(200)),
+              child: const Center(
+                  child: Text(
+                "Фильтр Чебышева\n3-ого порядка\nпередаёт\nпривет",
+                style: TextStyle(
+                    color: Color(0xFFF70153),
+                    fontSize: 35,
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.italic,
+                    fontFamily: "Arial"),
+                textAlign: TextAlign.center,
+              )),
+            )
+          ],
+        ));
   }
 }
 
