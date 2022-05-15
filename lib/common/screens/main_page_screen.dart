bdb0d83b5b86c57b4025ef4978af47cec36ab699
DPolikarpov <polikarpov.denv.2@gmail.com>
2022-05-15 21:33:23 +0300

    STASKS-15: Move mainpage to other file

---
+++ lib/common/screens/main_page_screen.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+const Color _appBarBackgroundColor = Color(0xFF121212);
+const Color _thirdContainerBoxDecorationColor = Color(0x4FFD0000);
+const String _thirdContainerText =
+    'Фильтр Чебышева\n3-ого порядка\nпередаёт\nпривет';
+const TextStyle _thirdContainerTextStyle = TextStyle(
+    color: Color(0xFFF70153),
+    fontSize: 35,
+    fontWeight: FontWeight.bold,
+    fontStyle: FontStyle.italic,
+    fontFamily: 'Arial');
+
+class MainPage extends StatefulWidget {
+  const MainPage({required this.title, Key? key}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  final containers = [
+    Container(height: 50, color: Colors.red),
+    Container(
+      height: 30,
+      child: const Text('Some text'),
+    ),
+    Container(
+      height: 200,
+      decoration: BoxDecoration(
+          color: _thirdContainerBoxDecorationColor,
+          borderRadius: BorderRadiusDirectional.circular(200)),
+      child: const Center(
+          child: Text(
+        _thirdContainerText,
+        style: _thirdContainerTextStyle,
+        textAlign: TextAlign.center,
+      )),
+    )
+  ];
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        backgroundColor: _appBarBackgroundColor,
+      ),
+      body: ListView.separated(
+          itemCount: 3,
+          itemBuilder: (context, index) => containers[index],
+          separatorBuilder: (context, index) => const SizedBox(height: 16)));
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'common/screens/main_page_screen.dart';
+
+const String _title = 'Best Music App EVER';
 
 void main() {
   runApp(const MyApp());
@@ -9,66 +12,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => MaterialApp(
-        title: _Constants.title,
+        title: _title,
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: const MyHomePage(title: _Constants.title),
+        home: const MainPage(title: _title),
       );
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({required this.title, Key? key}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final containers = [
-    Container(height: 50, color: Colors.red),
-    Container(
-      height: 30,
-      child: const Text('Some text'),
-    ),
-    Container(
-      height: 200,
-      decoration: BoxDecoration(
-          color: _Constants.thirdContainerBoxDecorationColor,
-          borderRadius: BorderRadiusDirectional.circular(200)),
-      child: const Center(
-          child: Text(
-        _Constants.thirdContainerText,
-        style: _Constants.thirdContainerTextStyle,
-        textAlign: TextAlign.center,
-      )),
-    )
-  ];
-
-  @override
-  Widget build(BuildContext context) => Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        backgroundColor: _Constants.appBarBackgroundColor,
-      ),
-      body: ListView.separated(
-          itemCount: 3,
-          itemBuilder: (context, index) => containers[index],
-          separatorBuilder: (context, index) => const SizedBox(height: 16)));
-}
-
-class _Constants {
-  static const String title = 'Best Music App EVER';
-  static const Color appBarBackgroundColor = Color(0xFF121212);
-  static const Color thirdContainerBoxDecorationColor = Color(0x4FFD0000);
-  static const String thirdContainerText =
-      'Фильтр Чебышева\n3-ого порядка\nпередаёт\nпривет';
-  static const TextStyle thirdContainerTextStyle = TextStyle(
-      color: Color(0xFFF70153),
-      fontSize: 35,
-      fontWeight: FontWeight.bold,
-      fontStyle: FontStyle.italic,
-      fontFamily: 'Arial');
 }
