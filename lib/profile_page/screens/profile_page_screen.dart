3b8d6fe4aebd4c0b7d359ef98853885611d3aa0c
DPolikarpov <polikarpov.denv.2@gmail.com>
2022-05-16 01:04:45 +0300

    STASKS-15: Change StateFull on Stateless

--- lib/profile_page/screens/profile_page_screen.dart
+++ lib/profile_page/screens/profile_page_screen.dart
@@ -1,26 +1,21 @@
 import 'package:flutter/material.dart';
 
 const Color _appBarBackgroundColor = Color(0xFF121212);
+const String _imageUrl = r'https://picsum.photos/250?image=9';
 
-class ProfilePage extends StatefulWidget {
+class ProfilePage extends StatelessWidget {
   const ProfilePage({required this.title, Key? key}) : super(key: key);
 
   final String title;
 
   @override
-  State<ProfilePage> createState() => _ProfilePageState();
-}
-
-class _ProfilePageState extends State<ProfilePage> {
-  final String imageUrl = r'https://picsum.photos/250?image=9';
-  @override
   Widget build(BuildContext context) => Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(title),
         backgroundColor: _appBarBackgroundColor,
       ),
       body: Container(
         alignment: Alignment.center,
-        child: Image.network(imageUrl),
+        child: Image.network(_imageUrl),
       ));
 }
