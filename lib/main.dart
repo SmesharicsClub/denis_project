ca4a34204ab0635d29819955910f7be5b8c2e429
DPolikarpov <polikarpov.denv.2@gmail.com>
2022-05-17 14:27:37 +0300

    STASKS-15: Rename profile_page_sreen to profile_page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 //import 'common/screens/main_page_screen.dart';
-import 'profile_page/screens/profile_page_screen.dart';
+import 'profile_page/screens/profile_page.dart';
 
 const String _title = 'Best Music App EVER';
 
