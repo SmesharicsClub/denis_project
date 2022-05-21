c549dd58053dd65b0f7ea7db8b6f873480fdb106
DPolikarpov <polikarpov.denv.2@gmail.com>
2022-05-21 23:25:15 +0300

    STASKS-28: Add sond card

---
+++ lib/common/widgets/song_card.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+import '../../best_music_app_icons.dart';
+import '../models/song.dart';
+
+class SongCard extends StatelessWidget {
+  const SongCard({required this.song, Key? key}) : super(key: key);
+
+  final Song song;
+
+  static const songCardColor = Color(0xFFF3E19F);
+  static const songCardHeight = 66.0;
+  static const songCardWidth = 358.0;
+  static const pauseIconSize = 36.0;
+
+  static const songCardShadow = BoxShadow(
+      offset: Offset(0, 1),
+      blurRadius: 20,
+      spreadRadius: 4,
+      color: Color(0x1A000000));
+
+  static const _musicTextTheme = TextStyle(
+      fontFamily: 'Inter',
+      fontWeight: FontWeight.w500,
+      fontStyle: FontStyle.normal,
+      fontSize: 15,
+      color: Color(0xFF000000));
+
+  static const _authorTextTheme = TextStyle(
+      fontFamily: 'Inter',
+      fontWeight: FontWeight.w300,
+      fontStyle: FontStyle.normal,
+      fontSize: 12,
+      color: Color(0xFF000000));
+
+  Widget get _pauseIcon => Container(
+        height: pauseIconSize,
+        width: pauseIconSize,
+        margin:
+            const EdgeInsets.only(left: 15, top: 15.5, right: 16, bottom: 14.5),
+        padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 10),
+        decoration: const BoxDecoration(
+            shape: BoxShape.circle, color: Color(0xFFF99E9E)),
+        child: const Icon(
+          BestMusicApp.pause,
+        ),
+      );
+
+  Widget get _musicInfo => Expanded(
+          child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+              margin: const EdgeInsets.only(top: 15),
+              child: Text(
+                song.name,
+                style: _musicTextTheme,
+              )),
+          Container(
+              margin: const EdgeInsets.only(top: 8, bottom: 10),
+              child: Text(
+                song.author,
+                style: _authorTextTheme,
+              ))
+        ],
+      ));
+
+  @override
+  Widget build(BuildContext context) => Container(
+        height: songCardHeight,
+        width: songCardWidth,
+        decoration: BoxDecoration(
+            color: songCardColor,
+            borderRadius: BorderRadiusDirectional.circular(16),
+            boxShadow: const [songCardShadow]),
+        child: Row(
+          children: [
+            _pauseIcon,
+            _musicInfo,
+            Container(
+              margin: const EdgeInsets.only(top: 21.5, right: 14, bottom: 20.5),
+              child: const Icon(BestMusicApp.menu),
+            )
+          ],
+        ),
+      );
+}
