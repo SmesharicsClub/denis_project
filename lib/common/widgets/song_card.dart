76864c31948bb8a223fc734c57f69c8cb5c455a7
DPolikarpov <polikarpov.denv.2@gmail.com>
2022-05-22 23:02:01 +0300

    STASKS-28: Remove static consts, build methods, hardcodding width

--- lib/common/widgets/song_card.dart
+++ lib/common/widgets/song_card.dart
@@ -5,75 +5,66 @@ import '../../best_music_app_icons.dart';
 class SongCard extends StatelessWidget {
   const SongCard({Key? key}) : super(key: key);
 
-  static const songCardColor = Color(0xFFF3E19F);
-  static const songCardHeight = 66.0;
-  static const songCardWidth = 358.0;
-  static const pauseIconSize = 36.0;
+  Color get _songCardColor => const Color(0xFFF3E19F);
+  double get _songCardHeight => 66.0;
+  double get _pauseIconSize => 36.0;
 
-  static const songCardShadow = BoxShadow(
-      offset: Offset(0, 1),
+  BoxShadow get _songCardShadow => BoxShadow(
+      offset: const Offset(0, 1),
       blurRadius: 20,
       spreadRadius: 4,
-      color: Color(0x1A000000));
-
-  static const _musicTextTheme = TextStyle(
-      fontFamily: 'Inter',
-      fontWeight: FontWeight.w500,
-      fontStyle: FontStyle.normal,
-      fontSize: 15,
-      color: Color(0xFF000000));
-
-  static const _authorTextTheme = TextStyle(
-      fontFamily: 'Inter',
-      fontWeight: FontWeight.w300,
-      fontStyle: FontStyle.normal,
-      fontSize: 12,
-      color: Color(0xFF000000));
-
-  Widget get _pauseIcon => Container(
-        height: pauseIconSize,
-        width: pauseIconSize,
-        margin:
-            const EdgeInsets.only(left: 15, top: 15.5, right: 16, bottom: 14.5),
-        padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 10),
-        decoration: const BoxDecoration(
-            shape: BoxShape.circle, color: Color(0xFFF99E9E)),
-        child: const Icon(
-          BestMusicApp.pause,
-        ),
-      );
-
-  Widget get _musicInfo => Expanded(
-          child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-              margin: const EdgeInsets.only(top: 15),
-              child: const Text(
-                'Музика - музика',
-                style: _musicTextTheme,
-              )),
-          Container(
-              margin: const EdgeInsets.only(top: 8, bottom: 10),
-              child: const Text(
-                'Исполнитель - полнитель',
-                style: _authorTextTheme,
-              ))
-        ],
-      ));
+      color: const Color(0x00000000).withOpacity(0.1));
 
   @override
   Widget build(BuildContext context) => Container(
-        height: songCardHeight,
-        width: songCardWidth,
+        height: _songCardHeight,
+        margin: const EdgeInsets.symmetric(horizontal: 8.5),
         decoration: BoxDecoration(
-            color: songCardColor,
+            color: _songCardColor,
             borderRadius: BorderRadiusDirectional.circular(16),
-            boxShadow: const [songCardShadow]),
+            boxShadow: [_songCardShadow]),
         child: Row(
           children: [
-            _pauseIcon,
-            _musicInfo,
+            Container(
+              height: _pauseIconSize,
+              width: _pauseIconSize,
+              margin: const EdgeInsets.only(
+                  left: 15, top: 15.5, right: 16, bottom: 14.5),
+              padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 10),
+              decoration: const BoxDecoration(
+                  shape: BoxShape.circle, color: Color(0xFFF99E9E)),
+              child: const Icon(
+                BestMusicApp.pause,
+              ),
+            ),
+            Expanded(
+                child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                    margin: const EdgeInsets.only(top: 15),
+                    child: const Text(
+                      'Музика - музика',
+                      style: TextStyle(
+                          fontFamily: 'Inter',
+                          fontWeight: FontWeight.w500,
+                          fontStyle: FontStyle.normal,
+                          fontSize: 15,
+                          color: Color(0xFF000000)),
+                    )),
+                Container(
+                    margin: const EdgeInsets.only(top: 8, bottom: 10),
+                    child: const Text(
+                      'Исполнитель - полнитель',
+                      style: TextStyle(
+                          fontFamily: 'Inter',
+                          fontWeight: FontWeight.w300,
+                          fontStyle: FontStyle.normal,
+                          fontSize: 12,
+                          color: Color(0xFF000000)),
+                    ))
+              ],
+            )),
             Container(
               margin: const EdgeInsets.only(top: 21.5, right: 14, bottom: 20.5),
               child: const Icon(BestMusicApp.menu),
