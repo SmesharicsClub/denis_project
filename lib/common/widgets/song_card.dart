94725aad8f66b2397d95c00f912f49a1e942fa93
DPolikarpov <polikarpov.denv.2@gmail.com>
2022-06-15 18:16:17 +0300

    STASKS-28: Remove unnecessary constants

--- lib/common/widgets/song_card.dart
+++ lib/common/widgets/song_card.dart
@@ -5,29 +5,25 @@ import '../../best_music_app_icons.dart';
 class SongCard extends StatelessWidget {
   const SongCard({Key? key}) : super(key: key);
 
-  Color get _songCardColor => const Color(0xFFF3E19F);
-  double get _songCardHeight => 66.0;
-  double get _pauseIconSize => 36.0;
-
-  BoxShadow get _songCardShadow => BoxShadow(
-      offset: const Offset(0, 1),
-      blurRadius: 20,
-      spreadRadius: 4,
-      color: const Color(0x00000000).withOpacity(0.1));
-
   @override
   Widget build(BuildContext context) => Container(
-        height: _songCardHeight,
+        height: 66.0,
         margin: const EdgeInsets.symmetric(horizontal: 8.5),
         decoration: BoxDecoration(
-            color: _songCardColor,
+            color: const Color(0xFFF3E19F),
             borderRadius: BorderRadiusDirectional.circular(16),
-            boxShadow: [_songCardShadow]),
+            boxShadow: [
+              BoxShadow(
+                  offset: const Offset(0, 1),
+                  blurRadius: 20,
+                  spreadRadius: 4,
+                  color: const Color(0x00000000).withOpacity(0.1))
+            ]),
         child: Row(
           children: [
             Container(
-              height: _pauseIconSize,
-              width: _pauseIconSize,
+              height: 36.0,
+              width: 36.0,
               margin: const EdgeInsets.only(
                   left: 15, top: 15.5, right: 16, bottom: 14.5),
               padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 10),
