ca104387e0f3dbce0c1637bc189b524951cd8d81
DPolikarpov <polikarpov.denv.2@gmail.com>
2022-06-23 19:36:16 +0300

    STASKS-30: Change containers' list to songs' list

--- lib/common/screens/main_page_constants.dart
+++
@@ -1,7 +0,0 @@
-class MainPageConstants {
-  static const firstContainerHeight = 50.0;
-  static const secondContainerHeight = 30.0;
-  static const thirdContainerHeight = 200.0;
-  static const thirdContainerBorderRadius = 200.0;
-  static const sizedBoxBetweenContainersHeight = 16.0;
-}
